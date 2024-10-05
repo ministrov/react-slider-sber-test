edb1e2bb655103cb567a6096a179aefc09acd8af
unknown <ministrov2018@gmail.com>
2024-10-05 14:11:45 +0300

    feat: write full of logic to change slides on clicking control buttons

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,15 +1,69 @@
+import { useCallback, useRef, useEffect } from 'react'
 import classNames from 'classnames'
+import { ISlide } from '../../utils/types'
 import previous from '../../utils/images/previous.png'
 import start from '../../utils/images/start.png'
 import pause from '../../utils/images/pause.png'
 import next from '../../utils/images/next.png'
 import styles from './Navigation.module.css'
 
-function Navigation() {
-  const handlePreviousSlide = () => {};
-  const handleStartSlide = () => {};
-  const handleStopSlide = () => {};
-  const handleNextSlide = () => {};
+type NavigationProps = {
+  setCurrentSlide: (setSlider: (currentSlide: number) => number) => void;
+  currentSlide: number;
+  slides: ISlide[];
+}
+
+function Navigation({ currentSlide, setCurrentSlide, slides }: NavigationProps) {
+  const slider = useRef<number | null>(null);
+
+  const handleNextSlide = useCallback(() => {
+    clearInterval(slider.current!)
+    slider.current = null
+    setCurrentSlide((currentSlide) => {
+      if (currentSlide < slides.length) {
+        return currentSlide + 1
+      } else {
+        return 1
+      }
+    })
+  }, [setCurrentSlide, slides.length])
+
+  const handlePreviousSlide = useCallback(() => {
+    clearInterval(slider.current!)
+    slider.current = null
+    if (currentSlide > 1) {
+      setCurrentSlide((currentSlide) => currentSlide - 1)
+    }
+  }, [currentSlide, setCurrentSlide])
+
+  const handleNextSlideAuto = useCallback(() => {
+    setCurrentSlide((currentSlide) => {
+      if (currentSlide < slides.length) {
+        return currentSlide + 1
+      } else {
+        return 1
+      }
+    })
+  }, [setCurrentSlide, slides.length])
+
+  const handleStartSlide = useCallback(() => {
+    if (!slider.current) {
+      slider.current = setInterval(() => handleNextSlideAuto(), 2000)
+    }
+  }, [handleNextSlideAuto])
+
+  const handleStopSlide = useCallback(() => {
+    clearInterval(slider.current!)
+    slider.current = null
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      clearInterval(slider.current!)
+      slider.current = null
+    }
+  }, [])
+
   return (
     <div className={styles['navigation']}>
       <div className={styles['navigation__row']}>
--- src/modules/main/Main.tsx
+++ src/modules/main/Main.tsx
@@ -1,16 +1,27 @@
-import SlideList from '../../components/slideList/SlideList'
-import Navigation from '../../components/navigation/Navigation'
-import {slides} from '../../utils/data'
+import { useEffect, useState } from 'react';
+import Navigation from '../../components/navigation/Navigation';
+import SlideList from '../../components/slideList/SlideList';
+import { slides } from '../../utils/data'
 import styles from './Main.module.css'
+import { ISlide } from '../../utils/types';
 
+// console.log(data);
 console.log(slides);
 
 function Main() {
+  const [slidesState, setSlidesState] = useState<ISlide[]>([]);
+  const [currentSlide, setCurrentSlide] = useState<number>(1);
+
+  console.log(slidesState);
+
+  useEffect(() => {
+    setSlidesState(slides);
+  }, []);
   return (
     <div className={styles['main']}>
       <div className={styles['main__row']}>
-        <SlideList slides={slides} currentSlide={1} />
-        <Navigation />
+        <SlideList slides={slidesState} currentSlide={currentSlide} />
+        <Navigation setCurrentSlide={setCurrentSlide} currentSlide={currentSlide} slides={slides} />
       </div>
     </div>
   )
